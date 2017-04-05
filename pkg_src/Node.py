51aaa0e54c1771cc5fbbc0202bca63baae821798
Nick Hollan <nchollan@wpi.edu>
2017-04-05 18:38:49 -0400

    Fixing merge errors

---
+++ pkg_src/Node.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+# -1 Obstacle
+# 0 Unexplored
+# 1 Explored
+# 2 Fronteir
+
+class Node(object):
+	
+	def __init__(self, x, y, state):
+		self.x = x
+		self.y = y
+		self.state = state
+		self.gCost = 0
+		self.hCost = 0
+		self.fCost = 0
+
+	#Uses Manhattan for G and Direct for H
+	def findF(self, start, end):
+		self.fCost = findG(start, self) + findH(self, end)
+		return  self.fCost
+
+	#finds the known distance using Manhattan
+	def findG(self, goal):
+		self.gcost = findManhattan(self, goal)
+		return self.gCost
+
+	#Finds the Heuristic Distance using direct
+	def findH(self, goal):
+		self.hCost = findDirect(self, goal)
+		return self.hCost
+
+	#determines distance between two nodes using the Manhattan distance
+	def findManhattan(self, nodeToCheck):
+		Xdif = np.absolute(nodeToCheck.x - self.x)
+		yDif = np.absolute(nodeToCheck.y - self.y)
+		return Xdif + yDif
+
+	#finds the distance between two nodes using the Direct Distance
+	def findDirect(self, nodeToCheck):
+		Xdif = nodeToCheck.x - self.x
+		yDif = nodeToCheck.y - self.y
+		val = np.sqrt(Xdif**2 + yDif**2)
+		return val
+
+		
+		
