c79f8371d07c7827f72399767550c0201b3a7ba6
Nick Hollan <nchollan@wpi.edu>
2017-04-05 17:23:05 -0400

    Edited Node.py

--- pkg_src/Node.py
+++ pkg_src/Node.py
@@ -2,31 +2,20 @@
 
 import numpy as np
 
-"""
-Start = 1
-Goal = 2
-Wall = 3
-Frontier = 4
-Explored = 5
-Unexplored = 6
-"""
-
 class Node(object):
 	
-	def __init__(self, parent, x, y):
-		self.parent = parent
+	def __init__(self, x, y):
 		self.x = x
 		self.y = y
 		self.gCost = 0
 		self.hCost = 0
 		self.fCost = 0
-		
-
 
 	#Uses Manhattan for G and Direct for H
 	def findF(self, start, end):
 		self.fCost = findG(start, self) + findH(self, end)
 		return  self.fCost
+
 	#finds the known distance using Manhattan
 	def findG(self, goal):
 		self.gcost = findManhattan(self, goal)
