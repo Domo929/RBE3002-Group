d62c695b072d898bbc18e793ee1b93e577165abd
Nick Hollan <nchollan@wpi.edu>
2017-04-05 20:52:11 -0400

    Fixing some A* Stuffs

--- pkg_src/Node.py
+++ pkg_src/Node.py
@@ -19,17 +19,17 @@ class Node(object):
 
 	#Uses Manhattan for G and Direct for H
 	def findF(self, start, end):
-		self.fCost = findG(start, self) + findH(self, end)
+		self.fCost = self.findG(start) + self.findH(end)
 		return  self.fCost
 
 	#finds the known distance using Manhattan
-	def findG(self, goal):
-		self.gcost = findManhattan(self, goal)
+	def findG(self, start):
+		self.gCost = self.findManhattan(start)
 		return self.gCost
 
 	#Finds the Heuristic Distance using direct
-	def findH(self, goal):
-		self.hCost = findDirect(self, goal)
+	def findH(self, end):
+		self.hCost = self.findDirect(end)
 		return self.hCost
 
 	#determines distance between two nodes using the Manhattan distance
