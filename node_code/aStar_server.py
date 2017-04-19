875594841814eabf1be3b9c194dff6a3fac21aef
nchollan <nchollan@gmail.com>
2017-04-18 20:43:39 -0400

    Added more on service

--- node_code/aStar_server.py
+++ node_code/aStar_server.py
@@ -5,8 +5,7 @@ from nav_msgs.msg import GridCells, Path
 from Node import Node
 from geometry_msgs.msg import Point, PoseStamped, PoseWithCovarianceStamped
 
-def aStarPathFinding(self, start, goal):
-	self.path=[]
+def aStarPathFinding(start, goal):
 	if(start.state==-1):
 		raise ValueError('Starting in a wall', start.x,start.y)
 	openset = set()
@@ -22,10 +21,10 @@ def aStarPathFinding(self, start, goal):
 				path.append(current)
 				current = current.parent
 			path.append(current)
-			return self.toPublishable(path[::-1])
+			return toPublishable(path[::-1])
 		openset.remove(current)
 		closedset.add(current)
-		for node in self.findChildren(current):
+		for node in findChildren(current):
 			if node in closedset or node.state == -1:
 				continue
 			if node in openset:
@@ -41,7 +40,7 @@ def aStarPathFinding(self, start, goal):
 		
 	raise ValueError('No Path Found', start.x, start.y, goal.x, goal.y)
 	print("Finished A*")
-def toPublishable(self,listOfNodes):
+def toPublishable(listOfNodes):
 	listOfPoints = []
 	for node in listOfNodes:
 		point = Point()
@@ -49,7 +48,7 @@ def toPublishable(self,listOfNodes):
 		point.y=node.y
 		listOfPoints.append(point)
 	return listOfPoints	
-def findChildren(self,currentNode):
+def findChildren(currentNode):
 	currentX=currentNode.x
 	currentY=currentNode.y
 	listOfNodes = []
@@ -58,7 +57,7 @@ def findChildren(self,currentNode):
 			if(not(x == 0 and y == 0)):
 				listOfNodes.append(self.nodes[currentX + x][currentY + y])
 	return listOfNodes
-def findChildrenRadius(self, currentNode, radius):
+def findChildrenRadius(currentNode, radius):
 	width = len(self.nodes)
 	height = len(self.nodes[0])
 	currentX=currentNode.x
@@ -76,31 +75,27 @@ def findChildrenRadius(self, currentNode, radius):
 			if(notSelf and notLargerThanGrid and notLessThanGrid):
 				listOfNodes.append(self.nodes[searchX][searchY])
 	return listOfNodes
-def convertToGridScaling(self,arr,resolution):
+def convertToGridScaling(arr):
+	#NEED TO ADD
+		#Resolution
+
 	for i in range(0, len(arr)):
 		arr[i].x=arr[i].x*resolution + 0.1
 		arr[i].y=arr[i].y*resolution + 0.2
 	return arr
-def returnPathWaypoints(self, start, end, inputMap, resolution):
-	self.nodes = inputMap
-	path=[]
-	pathReturned = self.aStarPathFinding(start,end)
-#create gridcell output for path
-	pathCell = GridCells()
-	pathCell.header.frame_id = "map"
-	pathCell.cell_width =resolution
-	pathCell.cell_height=resolution
-	pathCell.cells = self.convertToGridScaling(pathReturned,resolution)
-#create list of waypoints
+def returnPathWaypoints(start, end):
+	#NEED TO ADD
+		#Resolution
+
+	pathReturned = aStarPathFinding(start,end)
+
 	listOfWaypoints = []
 	for x in range(1, len(pathReturned)-1):  
 		currentNode = pathReturned[x]
 		nextNode = pathReturned[x+1]
 		previousNode = pathReturned[x-1]
-
 		angleToNextPoint = math.atan2((nextNode.y-currentNode.y),(nextNode.x-currentNode.x))
 		prevToCurrentAngle = math.atan2((currentNode.y-previousNode.y),(currentNode.x-previousNode.x))
-
 		if(prevToCurrentAngle!=angleToNextPoint):
 			quaternion = tf.transformations.quaternion_from_euler(0,0,angleToNextPoint)
 			tempPose = PoseStamped()
@@ -112,23 +107,29 @@ def returnPathWaypoints(self, start, end, inputMap, resolution):
 			tempPose.pose.orientation.z = quaternion[2]
 			tempPose.pose.orientation.w = quaternion[3]
 			listOfWaypoints.append(tempPose)
-	print("After For Loop")
+	tempPose = PoseStamped()
+	tempPose.header.frame_id = 'map'
+	tempPose.pose.position.x= pathReturned[-1].x
+	tempPose.pose.position.y= pathReturned[-1].y
+	listOfWaypoints.append(tempPose)
+
+	waypointsToPublish = Path()
+	waypointsToPublish.header.frame_id = 'map'
+	waypointsToPublish.poses = listOfWaypoints
+	print("Finished Getting Waypoints")
+	return listOfWaypoints
+
 
 def handle_aStar(req):
-	start=req.start
-	end=req.end
+	startPoint=req.start
+	endPoint=req.end
+	shortestPath=returnPathWaypoints(startPoint,endPoint)
+
 	return aStarResponse()
-	
 def aStar_server():
 	rospy.init_node('aStar_server')
 	s = rospy.Service('aStar', aStarCompute, handle_aStar)
 	rospy.spin()
-
 if __name__ == "__main__":
 	subMap = rospy.Subscriber('/map',OccupancyGrid,saveMap,queue_size=10)
 	aStar_server()
-
-
-
-
-#SETUP SERVICE
