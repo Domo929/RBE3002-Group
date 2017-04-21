b73622187ccf2565cf77f9b193e4e8cbab895a58
The Big M <mphopkins@wpi.edu>
2017-04-21 00:58:11 -0400

    class to find frontier centriods added
    this is not tested and cannot be run yet. However, the code is all written
    and the logic should work. If it will be used as a class then a blank
    constructer must be created as the class does not have any instance variables
    it is just a collection of methods that all take in params for any information
    they need
    
    -
    mike

---
+++ pkg_src/findingFrontiers.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+import rospy, tf, math, time
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import GridCells, Path
+from Node import Node
+from geometry_msgs.msg import Point, PoseStamped, PoseWithCovarianceStamped
+
+class aStar:
+	def findFrontierRegionCentriods(self,map,threshold):
+		frontierRegions = findFrontierRegions(map,threshold) #returns a list of lists of points
+		centriods = []
+
+		for region in frontierRegions:
+			tempSumX = 0
+			tempSumY = 0
+
+			for point in region:
+				tempSumX += point.x
+				tempSumY += point.y
+
+			centiord = Point()
+
+			centriod.y = int(tempSumY/float(len(region))) # find average of x and y
+			centriod.x = int(tempSumX/float(len(region))) # set this as the centriod and add to the list
+			centriods.append(centriod)
+
+		return centriods
+
+	def findFrontierRegions(self,map,threshold):
+		frontierRegions = []
+		listOfFrontiers = findFrontiers(map)
+
+		frontierRegions.append([listOfFrontiers.pop()]) #start the first region with the first frontier point
+
+		for counter in len(listOfFrontiers):
+			frontierPoint = listOfFrontiers.pop()
+			for region in frontierRegions:
+				for existingPoint in region:
+					if(isAdjacent(existingPoint,frontierPoint)): #if the point is adjacent to any other points add it
+						region.append(frontierPoint)			 #to that regions list of points
+					else:
+						frontierRegions.append([frontierPoint])  #if not then create a new region
+ 
+		for region in frontierRegions: #remove any regions that are smaller than the width of the robot
+			if(len(region)<threshold): #because they are insignifanct and do not need to be explored
+				frontierRegions.remove(region)
+
+		return frontierRegions
+
+	
+	def findFrontiers(self, map):
+		height = map.info.height
+		width = map.info.width
+		data = map.data
+
+		listOfFrontiers = []
+		point = Point()
+
+		#the logic here is that any known open cell adjacent
+		#to an unknown cell must be a frontier
+		for y in range(height):
+			for x in range(width):
+				currentDataPoint = data[x+y(width)]
+				if(currentDataPoint < 50):
+					if(isAdjacentToOpen(x,y,data,width)):
+						point = Point()
+						point.x = x
+						point.y = y
+
+						listOfFrontiers.append(point)
+
+		return listOfFrontiers
+
+
+	def isAdjacentToOpen(self,x,y,data,width):
+		for yAdd in range(-1,1):
+			for xAdd in range(-1,1):
+				xCheck = x+xAdd
+				yCheck = y+yAdd
+
+				if(data[xCheck+yCheck(width)==-1 and not(x == 0 and y==0)):
+					return True
+		return False
