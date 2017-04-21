f63f7eaca4babe9b6d286084659544a7f749d3eb
Domo929 <Dominic.cupo@gmail.com>
2017-04-20 22:25:49 -0400

    Formatting on tf broadcaster, stub of viisualize

--- node_code/turtle_tf_broadcaster.py
+++ node_code/turtle_tf_broadcaster.py
@@ -6,11 +6,9 @@ def handle_turtle_pose(msg):
     br = tf.TransformBroadcaster()
     pos = (msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z)
     ori = (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w)
-    br.sendTransform(pos,ori,rospy.Time.now(),'base_footprint','/odom')
+    br.sendTransform(pos, ori, rospy.Time.now(), 'base_footprint', '/odom')
 
 if __name__ == '__main__':
     rospy.init_node('turtle_tf_broadcaster')
-    rospy.Subscriber('/odom',
-                     Odometry,
-                     handle_turtle_pose)
+    rospy.Subscriber('/odom', Odometry, handle_turtle_pose)
     rospy.spin()
