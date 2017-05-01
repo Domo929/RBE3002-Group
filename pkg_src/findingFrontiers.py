aacb5c40be597960a2385da5d9630539d668e4c3
nchollan <nchollan@gmail.com>
2017-05-01 13:51:54 -0400

    All commented up, just needs a read me

--- pkg_src/findingFrontiers.py
+++ pkg_src/findingFrontiers.py
@@ -47,7 +47,9 @@ class FindFrontiers:
 		return centriods
 	"""
 	Takes in a map and a threshold, and finds a list of frontier regions
-		
+		Params: mapOG, threshold; mapOG is the OccupancyGrid for the map
+								  threshold is what determines whether a frontier region is big enough be worth exploring
+		Returns: frontierRegions; a list of frontier regions derived from the current map
 	"""
 	def findFrontierRegions(self,mapOG,threshold):
 		print "Finding Frontier Regions"
@@ -90,7 +92,11 @@ class FindFrontiers:
 				frontierRegions.remove(region)
 
 		return frontierRegions
-
+	"""
+	Takes in a map, and finds a list of frontier points
+		Params: mapOG; mapOG is the OccupancyGrid for the map
+		Returns: listOfFrontiers; a list of frontier points derived from the current map
+	"""
 	def findFrontiers(self, mapOG):
 		height = mapOG.info.height
 		width = mapOG.info.width
@@ -106,7 +112,6 @@ class FindFrontiers:
 						point = Point()
 						point.x = x
 						point.y = y
-
 						listOfFrontiers.append(point)
 
 		pathCell = GridCells()
@@ -114,12 +119,17 @@ class FindFrontiers:
 		pathCell.cell_width =mapOG.info.resolution
 		pathCell.cell_height=mapOG.info.resolution
 		pathCell.cells = self.convertGridToWorld(listOfFrontiers,mapOG.info.resolution, mapOG.info.origin)
-
-		#while(not(rospy.is_shutdown())):
 		print "publishing: Frontier"
 		pubFrontier.publish(pathCell)
-
 		return listOfFrontiers
+	"""
+	Takes in an x, y, and map data and determines whether or not the point is adjacent to an unexplored point on the map
+		Params: x, y, data, width; x is the x position of the point being checked
+								   y is the y position of the point being checked
+								   data is the map data of the current map
+								   width is the width of the map
+		Returns boolean; Is the point adjacent to an unexplored point
+	"""
 	def isAdjacentToOpen(self,x,y,data,width):
 		for yAdd in range(-1,2):
 			for xAdd in range(-1,2):
@@ -130,16 +140,24 @@ class FindFrontiers:
 				if(data[xCheck+yCheck*(width)]==-1 and not(xAdd == 0 and yAdd==0)):
 					return True
 		return False
+
+	"""
+	Takes in two points and determines if they are close enough to eachother that they belong in the same frontier region
+		Params: point1, point2; point1 is the first point
+								point2 is the point you are comparing to the first point
+		Returns boolean; Is the point adjacent to another frontier point
+	"""
 	def isAdjacent(self,point1,point2):
-		# for yAdd in range(-1,2):
-		# 	for xAdd in range(-1,2):
-		# 		# xCheck = point1.x+xAdd
-		# 		# yCheck = point1.y+yAdd
-		
 		if(math.sqrt((point1.x-point2.x)**2+(point1.y-point2.y)**2) < .1):
-			#print (math.sqrt((point1.x-point2.x)**2+(point1.y-point2.y)**2))
 			return True
 		return False
+	"""
+	Takes in an array, a resolution and the origin of the map
+		Params: arr, resolution, origin; arr is the map data
+										 resolution of the map
+										 origin of the map
+		Returns array; converted map
+	"""
 	def convertGridToWorld(self,arr,resolution, origin):
 	    for i in range(0, len(arr)):
 	        arr[i].x=arr[i].x*resolution+origin.position.x+(0.5*resolution)
